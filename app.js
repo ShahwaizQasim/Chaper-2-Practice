f92693aa4805d6582dbb9510500f67d470fda535
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-03-15 00:17:39 -0700

    ch 2 assignments done

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /* Question 1 */
 let username;
 
@@ -13,7 +12,16 @@ alert(message);
 let student_name = prompt("Enter Your Name");
 let student_age = prompt("Enter Your Age");
 let student_course = prompt("Enter Your Course");
-alert("Student Name: " + student_name + "\n" + "Student Age: " + student_age + "\n" + "Student Course: " + student_course);
+alert(
+  "Student Name: " +
+    student_name +
+    "\n" +
+    "Student Age: " +
+    student_age +
+    "\n" +
+    "Student Course: " +
+    student_course
+);
 
 /* Question 5 */
 alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
@@ -23,7 +31,8 @@ let email = "example@gmail.com";
 alert("Email: " + email);
 
 /* Question 7 */
-let book = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+let book =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 alert(book);
 
 /* Question 8 */
@@ -32,11 +41,3 @@ document.write("Yah! I can write HTML content through Javascript");
 /* Question 9 */
 let lines = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
 alert(lines);
-
-
-
-
-
-
-
-
